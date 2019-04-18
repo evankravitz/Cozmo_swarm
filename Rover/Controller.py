c49fd0023afb5aef25f4183686074505b3941e39
Evan Kravitz <esk95@cornell.edu>
2019-04-18 18:56:34 -0400

    modified controller script

--- Rover/Controller.py
+++ Rover/Controller.py
@@ -8,20 +8,23 @@ class Controller:
 		self.NUM_CUBES = num_cubes
 		self.cubes = [False for _ in range(self.NUM_CUBES)]
 		self.setup()
+		self.run()
 		
 	def setup(self):
 		os.system('mkfifo fifo_controller_read')
 		
 	def run(self):
 		while True:
-			output = subprocess.check_output('cat fifo controller_read').split(' ')
-			robot_id = int(output[0])
-			cube_num = int(output[1])
-			if not self.cubes[cube_num]:
-				self.cubes[cube_num] = True
-				os.system('echo 1 > fifo_' + robot_id + '_read')
-			else:
-				os.system('echo 0 > fifo_' + robot_id + '_read')
+			output = subprocess.check_output(['cat', 'fifo_controller_read']).decode('UTF-8').split()
+			print(int(output[0]))
+			print(int(output[1]))
+		#	robot_id = int(output[0])
+	#		cube_num = int(output[1])
+#			if not self.cubes[cube_num]:#
+#				self.cubes[cube_num] = True
+#				os.system('echo 1 > fifo_' + robot_id + '_read')
+#			else:
+#				os.system('echo 0 > fifo_' + robot_id + '_read')
 	
 		
 
