60575b1b7fb226cd165f3a825b7c4a9c83fbb8fb
Evan Kravitz <esk95@cornell.edu>
2019-04-22 18:41:47 -0400

    new changes

--- Rover/Controller_communication_test.py
+++ Rover/Controller_communication_test.py
@@ -6,7 +6,7 @@ import os
 
 
 if __name__ == "__main__":
-	os.system('bash get_permission_to_pickup_cube.sh 10.148.12.120 0 0')
-	output = subprocess.check_output(['bash', 'get_response_from_controller.sh', '10.148.12.120', '0'])
+	os.system('bash get_permission_to_pickup_cube.sh 10.148.11.163 0 1')
+	output = subprocess.check_output(['bash', 'get_response_from_controller.sh', '10.148.11.163', '0']).decode('UTF-8')
 	print(int(output[-2]))
 
